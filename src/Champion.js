180dacb6dba74608e90e5bf015e8f60e873b34f0
David Adams <daadams@gannett.com>
2014-12-17 09:26:26 -0500

    Reducing unnecessary code in unit classes

--- src/Champion.js
+++ src/Champion.js
@@ -14,9 +14,8 @@ this.nudge2d = this.nudge2d || {}; // Initialize namespace
   Champion.prototype.initialize = function(){
     var animation = nudge2d.AssetManifest.champ;
     var spritesheet = new createjs.SpriteSheet(animation);
-    this.activeState = "idle";
 
-    this.Sprite_initialize(spritesheet, this.activeState);
+    this.Sprite_initialize(spritesheet, "idle");
 
     // Default properties.
     this.primWeapon = "blue_bullet";
@@ -27,13 +26,15 @@ this.nudge2d = this.nudge2d || {}; // Initialize namespace
    * @param {String} state the new animation
    */
   Champion.prototype.setAnimation = function(state){
-    this.activeState = state;
+    if(state != this.currentAnimation){
+      this.gotoAndPlay(state);
+    }
   };
 
   // Fires unit's primary weapon.
   Champion.prototype.firePrimary = function(){
     this.setAnimation("shoot");
-    var bullet = new nudge2d.Projectile(this.primWeapon, 30, 0);
+    var bullet = new nudge2d.Projectile(this.primWeapon, 30, 0, 5);
     bullet.x = this.x+30;
     bullet.y = this.y+35;
     // Add the bullet to the scene.
@@ -44,12 +45,7 @@ this.nudge2d = this.nudge2d || {}; // Initialize namespace
    * Advance the animation of the Champion unit
    * @param stage a reference to the parent stage
    */
-  Champion.prototype.tick = function(stage){
-    if(this.activeState != this.currentAnimation){
-      this.gotoAndPlay(this.activeState);
-    }
-
-  };
+  Champion.prototype.tick = function(stage){};
 
   nudge2d.Champion = Champion;
 }(window));
--- src/enemy.js
+++ src/enemy.js
@@ -14,27 +14,18 @@ this.nudge2d = this.nudge2d || {}; // Initialize namespace
   Enemy.prototype.initialize = function(sprite){
     var animation = nudge2d.AssetManifest[sprite];
     var spritesheet = new createjs.SpriteSheet(animation);
-    this.activeState = "idle";
 
-    this.Sprite_initialize(spritesheet, this.activeState);
+    this.Sprite_initialize(spritesheet, "idle");
     this.health = 100;
   };
 
   /**
    * Changes the current animation of the unit
    * @param {String} state the new animation
-   * @param onComplete callback after animation completes
    */
-  Enemy.prototype.setAnimation = function(animation, onComplete){
-    var callback = onComplete || function(){};
-    this.activeState = animation;
-    this.on("animationend", callback);
-  };
-
-  Enemy.prototype.notifyHit = function(target){
-    if(target instanceof Projectile){
-      this.health -= target.damage;
-      nudge2d.Scene.removeNode(target);
+  Enemy.prototype.setAnimation = function(state){
+    if(state != this.currentAnimation){
+      this.gotoAndPlay(state);
     }
   };
 
@@ -43,15 +34,11 @@ this.nudge2d = this.nudge2d || {}; // Initialize namespace
    * @param stage a reference to the parent stage
    */
   Enemy.prototype.tick = function(stage){
-    if(this.activeState != this.currentAnimation){
-      this.gotoAndPlay(this.activeState);
-    }
-
     // Enemy killed animation
-    if(this.health <= 0)
-      this.setAnimation("dead", function(){
-        nudge2d.Scene.removeNode(this);
-      });
+    if(this.health <= 0){
+      this.setAnimation("dead");
+      this.on("animationend", this.stop)
+    }
   };
 
   nudge2d.Enemy = Enemy;
